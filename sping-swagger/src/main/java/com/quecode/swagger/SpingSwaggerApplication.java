0512082acc2216ed3883470218af8da04c32848c
quedevel <kdh90817@naver.com>
2022-01-30 19:24:01 +0900

    springboot-swagger3 init

---
+++ sping-swagger/src/main/java/com/quecode/swagger/SpingSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.quecode.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingSwaggerApplication.class, args);
+    }
+
+}
