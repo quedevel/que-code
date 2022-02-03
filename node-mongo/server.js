763584e836d7cf46ba898d542e34c9b7323bdfe9
quedevel <kdh90817@naver.com>
2022-02-03 22:38:40 +0900

    express post 요청

--- node-mongo/server.js
+++ node-mongo/server.js
@@ -1,22 +1,30 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-app.listen(8080, function(){
+app.use(bodyParser.urlencoded({extended : true}))
+
+app.listen(8080, () => {
     console.log('listening on 8080')    
 })
 
-app.get('/pet', function(req, res){
+app.get('/pet', (req, res) => {
     res.send('pet....')
 })
 
-app.get('/beauty', function(req, res){
+app.get('/beauty', (req, res) => {
     res.send('beauty....')
 })
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.sendFile(__dirname+'/index.html')
 })
 
-app.get('/write', function(req, res) { 
+app.get('/write', (req, res) => { 
     res.sendFile(__dirname +'/write.html')
-});
+})
+
+app.post('/add', (req, res) => {
+    res.send('post success....')
+    console.log(req.body)
+})
