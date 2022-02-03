d9daabf049cbfeb7b17d363a23eccec4d94ec7f2
quedevel <kdh90817@naver.com>
2022-02-03 21:51:21 +0900

    spring-oauth init

---
+++ spring-oauth/src/test/java/com/quecode/springoauth/SpringOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quecode.springoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
