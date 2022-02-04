ae4202e9838f418152c160ddd343236a06669115
quedevel <kdh90817@naver.com>
2022-02-04 14:10:31 +0900

    core javascript navi 작업

--- que-core-javascript/server.js
+++ que-core-javascript/server.js
@@ -4,3 +4,13 @@ const app = express();
 app.listen(8080, function() {
   console.log('listening on 8080');
 })
+
+app.get('/', (req, res) => { res.sendFile(__dirname+'/index.html') });
+
+app.get('/chapter/1', (req, res) => { res.sendFile(__dirname+'/html/immutableObject.html') })
+app.get('/chapter/2', (req, res) => { res.sendFile(__dirname+'/html/executionContext.html') })
+app.get('/chapter/3', (req, res) => { res.sendFile(__dirname+'/html/this.html') })
+app.get('/chapter/4', (req, res) => { res.sendFile(__dirname+'/html/callbackFunction.html') })
+app.get('/chapter/5', (req, res) => { res.sendFile(__dirname+'/html/closure.html') })
+app.get('/chapter/6', (req, res) => { res.sendFile(__dirname+'/html/prototype.html') })
+app.get('/chapter/7', (req, res) => { res.sendFile(__dirname+'/html/class.html') })
