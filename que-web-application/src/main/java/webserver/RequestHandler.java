cda9d841920d82d3cc7a7a0866bcf0f8c2f33589
quedevel <kdh90817@naver.com>
2022-02-07 22:45:58 +0900

    정적 파일 content_type 맞추기

--- que-web-application/src/main/java/webserver/RequestHandler.java
+++ que-web-application/src/main/java/webserver/RequestHandler.java
@@ -25,23 +25,34 @@ public class RequestHandler extends Thread {
 
             // bufferedReader.readLine() 첫번째 라인에 url 주소가 있음
             String url = bufferedReader.readLine().split(" ")[1];
+            String contentType = "";
+            System.out.println("0  "+url);
+            int idx = 1;
+            while (bufferedReader.ready()){
+                String temp = bufferedReader.readLine();
+                if(temp.indexOf("Accept") >= 0){
+                    contentType = temp.split(",")[0].split(" ")[1];
+                    break;
+                }
+            }
 
             DataOutputStream dos = new DataOutputStream(out);
 
             // body에 경로 넣기
             byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
 
-            response200Header(dos, body.length);
+            // js, css etc... content_type 맞추기
+            response200Header(dos, body.length, contentType);
             responseBody(dos, body);
         } catch (IOException e) {
             e.getStackTrace();
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: "+contentType+";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
