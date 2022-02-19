cd00009dab21fc5ace56b97cf8a60494e57d9f55
quedevel <kdh90817@naver.com>
2022-02-19 14:39:21 +0900

    로그인 페이지 만들기

--- node-mongo/server.js
+++ node-mongo/server.js
@@ -105,3 +105,40 @@ app.put('/edit',(req, res)=>{
         res.redirect('/list');
     })
 })
+
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const session = require('express-session')
+
+app.use(session({secret:'비밀코드', resave : true, saveUninitialized : false}))
+app.use(passport.initialize())
+app.use(passport.session());
+
+app.get('/login', (req, res) => { 
+    res.render('login.ejs')
+})
+
+app.post('/login', passport.authenticate('local', {
+    failureRedirect : '/fail'
+}), (req, res)=>{
+    res.redirect('/')
+})
+
+passport.use(new LocalStrategy({
+    usernameField: 'id', // form name 
+    passwordField: 'pw', // form name
+    session: true, //  session에 저장
+    passReqToCallback: false, // 아이디/비번 말고도 다른 정보 검사
+  }, function (입력한아이디, 입력한비번, done) {
+    //console.log(입력한아이디, 입력한비번);
+    db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+      if (에러) return done(에러)
+  
+      if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+      if (입력한비번 == 결과.pw) {
+        return done(null, 결과)
+      } else {
+        return done(null, false, { message: '비번틀렸어요' })
+      }
+    })
+  }));
