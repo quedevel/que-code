2565b87ceef27d8d7040bc77a6b0998c4f8ec61d
quedevel <kdh90817@naver.com>
2022-02-21 20:34:00 +0900

    작성자만 삭제 가능 처리

--- node-mongo/server.js
+++ node-mongo/server.js
@@ -22,9 +22,7 @@ require('dotenv').config()
 var db;
 
 MongoClient.connect(process.env.DB_URL, function (error, client) {
-    if (error) {
-        return console.log(console.log(error));
-    }
+    if (error)  return console.log(console.log(error));
 
     db = client.db("todoapp");
 
@@ -38,10 +36,10 @@ const LocalStrategy = require("passport-local").Strategy;
 const session = require("express-session");
 
 app.use(session({
-    secret: "비밀코드",
-    resave: true,
-    saveUninitialized: false,
-})
+        secret: "비밀코드",
+        resave: true,
+        saveUninitialized: false,
+    })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -83,7 +81,6 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((param, done) => {
-    console.log('param.... :'+param);
     db.collection('login').findOne({ id: param }, (error, result) => {
         done(null, result);
     })
@@ -106,138 +103,82 @@ app.get("/write", isLogin, (req, res) => {
 });
 
 app.post("/add", isLogin, (req, res) => {
-    console.log(req.user);
     db.collection("counter").findOne({ name: "board" }, (err, result) => {
-        var totalPost = result.totalPost;
-        db.collection("post").insertOne(
-            {
-                _id: totalPost + 1,
-                title: req.body.title,
-                date: req.body.date,
-                content: req.body.content,
-                writer: req.user._id,
-            },
-            (err, result) => {
-                console.log("저장완료!");
-                db.collection("counter").updateOne(
-                    {
-                        name: "board",
-                    },
-                    {
-                        $inc: {
-                            totalPost: 1,
-                        },
-                    },
-                    (err, result) => {
-                        if (err) return console.log(err);
-                        console.log("총갯수 +1 성공");
-                        res.redirect("/list");
-                    }
-                );
-            }
-        );
-    }
-    );
+        let totalPost = result.totalPost;
+        let param = {
+            _id: totalPost + 1,
+            title: req.body.title,
+            date: req.body.date,
+            content: req.body.content,
+            writer: req.user._id,
+        }
+        db.collection("post").insertOne( param, (err, result) => {
+            db.collection("counter").updateOne( { name: "board" }, { $inc: { totalPost: 1 } }, (err, result) => {
+                if (err) return console.log(err);
+                res.redirect("/list");
+            });
+        });
+    });
 });
 
 app.get("/list", function (req, res) {
     // 데이터 모두 찾기
-    db.collection("post")
-        .find()
-        .toArray((error, result) => {
-            res.render("list.ejs", {
-                posts: result,
-            });
-        });
+    db.collection("post").find().toArray((error, result) => {
+        res.render("list.ejs", { posts: result });
+    });
 });
 
 // URL parameter
 app.get("/detail/:id", function (req, res) {
-    db.collection("post").findOne(
-        {
-            _id: Number.parseInt(req.params.id),
-        },
-        (error, result) => {
-            console.log(result);
-            res.render("detail.ejs", {
-                board: result,
-            });
-        }
-    );
+    db.collection("post").findOne( { _id: Number.parseInt(req.params.id) }, (error, result) => {
+        res.render("detail.ejs", { board: result });
+    });
 });
 
 app.get("/edit/:id", function (req, res) {
-    db.collection("post").findOne(
-        {
-            _id: Number.parseInt(req.params.id),
-        },
-        (error, result) => {
-            console.log(result);
-            res.render("edit.ejs", {
-                board: result,
-            });
-        }
-    );
+    let param = {_id: Number.parseInt(req.params.id)}
+    db.collection("post").findOne(param, (error, result) => {
+        if (error) return console.log(error);
+        res.render("edit.ejs", { board: result });
+    });
 });
 
 app.delete("/delete", (req, res) => {
-    db.collection("post").deleteOne(
-        {
-            _id: Number.parseInt(req.body._id),
-        },
-        (err, result) => {
-            if (err) return console.log(err);
-            res.status(200).send({
-                message: "삭제에 성공했습니다.",
-            });
-        }
-    );
+    console.log(req.user)
+    let param = {_id: Number.parseInt(req.body._id), writer : req.user._id}
+    db.collection("post").deleteOne( param , (error, result) => {
+        if (error) return console.log(error);
+        res.status(200).send({ message: "삭제에 성공했습니다." });
+    });
 });
 
 app.put("/edit", (req, res) => {
-    var _id = Number.parseInt(req.body._id);
-
-    var params = {
+    let _id = Number.parseInt(req.body._id);
+    let params = {
         title: req.body.title,
         date: req.body.date,
         content: req.body.content,
     };
-
-    db.collection("post").updateOne(
-        {
-            _id: _id,
-        },
-        {
-            $set: params,
-        },
-        () => {
-            console.log("수정완료");
-            res.redirect("/list");
-        }
-    );
+    db.collection("post").updateOne( { _id: _id }, { $set: params }, () => {
+        res.redirect("/list");
+    });
 });
 
 
 
 app.get("/mypage", isLogin, (req, res) => {
-    console.log(req.user)
     res.render('mypage.ejs')
 });
 
 function isLogin(req, res, next) {
-    if (req.user) {
-        next();
-    } else {
-        res.send("로그인 안했는뎅?");
-    }
+    if (req.user)  next();
+    else res.send("로그인 안했는뎅?");
 }
 
 // URL query string
 app.get('/search', (req, res) => {
-    console.log(req.query.value)
-
-    var searchCondition = [
-        {
+    let searchCondition = [
+        {  
             $search: {
                 index: 'titleSearch',
                 text: {
@@ -250,7 +191,6 @@ app.get('/search', (req, res) => {
     ];
 
     db.collection('post').aggregate(searchCondition).toArray((error, result) => {
-        console.log(result);
         res.render("search.ejs", { posts: result });
     })
 })
@@ -263,7 +203,6 @@ app.post('/register', (req, res)=>{
     db.collection('login').findOne({id: req.body.id}, (error, result)=>{
         if(typeof result === null){
             db.collection('login').insertOne({id: req.body.id, pw: req.body.pw}, (error, result)=>{
-                console.log('회원가입 성공');
                 res.redirect('/');
             })
         } else {
