abc13b1bc78cf37f50cdf3a7a6624bdf627b89d9
quedevel <kdh90817@naver.com>
2022-02-27 01:50:46 +0900

    Multi Module 프로젝트 init

---
+++ que-multi-module/backoffice/src/main/java/org/quecode/BackofficeApplication.java
@@ -0,0 +1,11 @@
+package org.quecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofficeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackofficeApplication.class);
+    }
+}
