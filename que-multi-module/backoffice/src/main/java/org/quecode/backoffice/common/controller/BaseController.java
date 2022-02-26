07f4bf1f11751dddb46cdba6f65a52d4f4091975
quedevel <kdh90817@naver.com>
2022-02-27 02:08:00 +0900

    BaseController.java 위치 이동

--- que-multi-module/backoffice/src/main/java/org/quecode/backoffice/common/controller/BaseController.java
+++ que-multi-module/backoffice/src/main/java/org/quecode/backoffice/common/controller/BaseController.java
@@ -1,4 +1,4 @@
-package org.quecode.backoffice.common;
+package org.quecode.backoffice.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
