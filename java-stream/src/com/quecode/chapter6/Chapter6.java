73c57d13ef1a00227f7083ac622e3c4e02fdd20d
quedevel <kdh90817@naver.com>
2022-03-07 21:50:37 +0900

    Chapter6 init

---
+++ java-stream/src/com/quecode/chapter6/Chapter6.java
@@ -0,0 +1,17 @@
+package com.quecode.chapter6;
+
+/**
+ * 6.1 Stream
+ * 6.2 Filter
+ * 6.3 Map
+ * 6.4 Stream의 구성 요소
+ * 6.5 데이터 정렬
+ * 6.6 중복제거
+ * 6.7 FlatMap
+ */
+public class Chapter6 {
+
+    public static void main(String[] args) {
+
+    }
+}
