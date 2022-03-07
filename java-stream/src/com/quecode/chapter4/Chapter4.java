35193581f9efda994a225155c48a3fe9d4b45413
quedevel <kdh90817@naver.com>
2022-03-07 20:48:21 +0900

    4.3 BiConsumer

--- java-stream/src/com/quecode/chapter4/Chapter4.java
+++ java-stream/src/com/quecode/chapter4/Chapter4.java
@@ -2,6 +2,7 @@ package com.quecode.chapter4;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -29,6 +30,7 @@ public class Chapter4 {
 
         System.out.println();
         System.out.println();
+        
         /**
          * 4.2 Consumer
          */
@@ -39,7 +41,22 @@ public class Chapter4 {
         List<Integer> integerList = Arrays.asList(4,2,3);
 
         process(integerList, integerConsumer);
+        System.out.println();
+        System.out.println();
 
+        /**
+         * 4.3 BiConsumer
+         */
+        BiConsumer<Integer, Double> doubleBiConsumer = (index, input) -> System.out.println("Processing " + input+" at index "+index);
+        List<Double> doubleList = Arrays.asList(4.1,2.2,3.3);
+
+        process(doubleList, doubleBiConsumer);
+        System.out.println();
+        System.out.println();
+
+        /**
+         * 4.4 Predicate
+         */
     }
 
     public static void printRandomDoubles(Supplier<Double> randomSupplier, int count){
@@ -51,6 +68,12 @@ public class Chapter4 {
     public static <T> void process(List<T> inputs, Consumer<T> processor){
         for (T input : inputs){
             processor.accept(input);
+        }
+    }
+
+    public static <T> void process(List<T> inputs, BiConsumer<Integer, T> processor){
+        for (int i = 0; i < inputs.size(); i++) {
+            processor.accept(i, inputs.get(i));
         }
     }
 }
