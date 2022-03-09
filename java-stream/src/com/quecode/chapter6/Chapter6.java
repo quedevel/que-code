5b72b792e3789cda11ad8b5bb1f3618cbecef675
quedevel <kdh90817@naver.com>
2022-03-09 14:39:20 +0900

    6.1 Stream

--- java-stream/src/com/quecode/chapter6/Chapter6.java
+++ java-stream/src/com/quecode/chapter6/Chapter6.java
@@ -1,5 +1,12 @@
 package com.quecode.chapter6;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * 6.1 Stream
  * 6.2 Filter
@@ -12,6 +19,22 @@ package com.quecode.chapter6;
 public class Chapter6 {
 
     public static void main(String[] args) {
+        /**
+         * 6.1 Stream
+         */
+        Stream<String> nameStream = Stream.of("Alice", "Bob", "Charlie");
+        List<String> names = nameStream.collect(Collectors.toList());
+        System.out.println("names = " + names);
+
+        String[] cityArray = new String[]{"San Jose", "Seoul", "Tokyo"};
+        Stream<String> cityStream = Arrays.stream(cityArray);
+        List<String> cityList = cityStream.collect(Collectors.toList());
+        System.out.println("cityList = " + cityList);
+
+        Set<Integer> numberSet = new HashSet<>(Arrays.asList(3,5,7));
+        Stream<Integer> numberStream = numberSet.stream();
+        List<Integer> numberList = numberStream.collect(Collectors.toList());
+        System.out.println("numberList = " + numberList);
 
     }
 }
