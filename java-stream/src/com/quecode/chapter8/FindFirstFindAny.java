42eea4ebe27d0d9371d042e204a082f34479ca9e
quedevel <kdh90817@naver.com>
2022-03-14 20:17:21 +0900

    8.3 Find First Find Any

--- java-stream/src/com/quecode/chapter8/FindFirstFindAny.java
+++ java-stream/src/com/quecode/chapter8/FindFirstFindAny.java
@@ -1,9 +1,17 @@
 package com.quecode.chapter8;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class FindFirstFindAny {
     public static void main(String[] args) {
         /**
          * 8.3 Find First Find Any
          */
+        Optional<Integer> any = Stream.of(3, 2, -5, 6).filter(x -> x < 0).findAny();
+        System.out.println("any = " + any.get());
+
+        Optional<Integer> first = Stream.of(3, 2, -5, 6).filter(x -> x > 0).findFirst();
+        System.out.println("first = " + first.get());
     }
 }
