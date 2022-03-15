fa03d1cf0e21484b36c1fab0e6807e1ebef6bdc0
quedevel <kdh90817@naver.com>
2022-03-15 19:08:38 +0900

    불필요한 import문 제거

--- que-hello-core/core/src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ que-hello-core/core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -1,6 +1,5 @@
 package hello.core.singleton;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class StatefulServiceTest {
 
