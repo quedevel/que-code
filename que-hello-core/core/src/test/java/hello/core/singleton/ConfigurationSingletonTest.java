114ba04f4290579457ad77196f91b7999c27a87c
quedevel <kdh90817@naver.com>
2022-03-15 19:47:20 +0900

    ConfigurationSingletonTest 추가

---
+++ que-hello-core/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,37 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    @DisplayName("싱글톤 인스턴스 테스트")
+    void configurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository1 = " + memberRepository1);
+        System.out.println("orderService -> memberRepository2 = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberRepository).isSameAs(memberRepository1);
+        assertThat(memberRepository).isSameAs(memberRepository2);
+        assertThat(memberRepository1).isSameAs(memberRepository2);
+    }
+
+}
