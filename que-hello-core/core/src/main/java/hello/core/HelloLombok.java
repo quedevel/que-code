ecf09e1c295c70f130ea8a9cc40fea5287dcff77
quedevel <kdh90817@naver.com>
2022-03-17 19:33:00 +0900

    lombok 추가

---
+++ que-hello-core/core/src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+public class HelloLombok {
+
+    private String name;
+    private int age;
+}
