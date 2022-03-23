73c4f21209743618e8e496048e5ef4f164c74c1d
quedevel <kdh90817@naver.com>
2022-03-23 21:00:00 +0900

    @BeforeAll 테스트

--- que-multi-module/backoffice/src/test/java/org/quecode/backoffice/admin/AdminTest.java
+++ que-multi-module/backoffice/src/test/java/org/quecode/backoffice/admin/AdminTest.java
@@ -1,19 +1,29 @@
 package org.quecode.backoffice.admin;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.quecode.backoffice.admin.dto.AdminDTO;
+import org.quecode.backoffice.admin.entity.Admin;
 import org.quecode.backoffice.admin.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AdminTest {
 
     @Autowired
     private AdminService adminService;
 
-    @BeforeEach
+    /**
+     * 모든 테스트 시작전에 test123
+     */
+    @BeforeAll
     void deleteAdmin(){
         String adminId = "test123";
         adminService.deleteByAdminId(adminId);
@@ -22,6 +32,13 @@ public class AdminTest {
 
     @Test
     void join(){
-        adminService.join(AdminDTO.builder().build());
+        // given
+        AdminDTO dto = AdminDTO.builder().adminNm("test123").adminId("test123").adminPw("test123").build();
+
+        // when
+        Admin admin = adminService.join(dto);
+
+        // then
+        assertThat(admin.getAdminId()).isEqualTo(dto.getAdminId());
     }
 }
