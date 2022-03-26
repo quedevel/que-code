34256069f8afda886702a1e3e081758aeb57970b
quedevel <kdh90817@naver.com>
2022-03-27 00:54:57 +0900

    tree.root 설정

--- que-multi-module/backoffice/src/test/java/org/quecode/backoffice/code/CodeTests.java
+++ que-multi-module/backoffice/src/test/java/org/quecode/backoffice/code/CodeTests.java
@@ -1,8 +1,15 @@
 package org.quecode.backoffice.code;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.quecode.backoffice.common.constants.CommonConstants;
+import org.quecode.common.code.dto.CodeDTO;
+import org.quecode.common.code.entity.Code;
 import org.quecode.common.code.service.CodeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 public class CodeTests {
@@ -10,5 +17,16 @@ public class CodeTests {
     @Autowired
     private CodeService codeService;
 
-
+    @Test
+    void insertRoot(){
+//        CodeDTO codeDTO = CodeDTO.builder()
+//                .id(CommonConstants.TREE_ROOT)
+//                .parentId(CommonConstants.TREE_ROOT)
+//                .codeNm("tree.root")
+//                .depth(0).build();
+//
+//        Code code = codeService.insertCode(codeDTO);
+//
+//        assertThat(code.getId()).isEqualTo(codeDTO.getId());
+    }
 }
--- que-multi-module/common/src/main/java/org/quecode/common/code/service/CodeService.java
+++ que-multi-module/common/src/main/java/org/quecode/common/code/service/CodeService.java
@@ -1,6 +1,8 @@
 package org.quecode.common.code.service;
 
 import lombok.RequiredArgsConstructor;
+import org.quecode.common.code.dto.CodeDTO;
+import org.quecode.common.code.entity.Code;
 import org.quecode.common.code.repository.CodeRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,8 @@ import org.springframework.stereotype.Service;
 public class CodeService {
 
     private final CodeRepository codeRepository;
+
+    public Code insertCode(CodeDTO codeDTO){
+        return codeRepository.save(codeDTO.toEntity());
+    }
 }
