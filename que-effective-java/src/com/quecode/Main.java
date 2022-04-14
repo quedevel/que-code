83e1bf08951d9ee20dd7236b4729aebfbb70c875
quedevel <kdh90817@naver.com>
2022-04-14 14:23:10 +0900

    Effective Java 3/E init

---
+++ que-effective-java/src/com/quecode/Main.java
@@ -0,0 +1,7 @@
+package com.quecode;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("테스트");
+    }
+}
---
+++ que-effective-java/src/com/quecode/chapter2/Item1.java
@@ -0,0 +1,11 @@
+package com.quecode.chapter2;
+
+/**
+ * Description
+ * 1. 생성자 대신 정적 팩터리 메서드를 고려하라.
+ */
+public class Item1 {
+    public static void main(String[] args) {
+
+    }
+}
