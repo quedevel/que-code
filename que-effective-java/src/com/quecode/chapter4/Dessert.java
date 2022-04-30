7620a9e2fd8f88bf3fbd6b1f6b90cd9f62efc7d0
quedevel <kdh90817@naver.com>
2022-04-30 15:07:28 +0900

    item 25

---
+++ que-effective-java/src/com/quecode/chapter4/Dessert.java
@@ -0,0 +1,9 @@
+package com.quecode.chapter4;
+
+class Utensil {
+    static final String NAME = "pot";
+}
+
+class Dessert {
+    static final String NAME = "pie";
+}
---
+++ que-effective-java/src/com/quecode/chapter4/Utensil.java
@@ -0,0 +1,9 @@
+package com.quecode.chapter4;
+
+class Utensil {
+    static final String NAME = "pan";
+}
+
+class Dessert {
+    static final String NAME = "cake";
+}
