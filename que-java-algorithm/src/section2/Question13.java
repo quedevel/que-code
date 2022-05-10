1e7186aca074275081922cc8a99858a24d233e1e
quedevel <kdh90817@naver.com>
2022-05-10 16:57:36 +0900

    Question13

--- que-java-algorithm/src/section2/Question13.java
+++ que-java-algorithm/src/section2/Question13.java
@@ -1,8 +1,10 @@
+package section2;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Question13 {
     public static void solution(List<Integer> list){
         String result = "";
         int prev = 0;
