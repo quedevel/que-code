b85e5b228c33722fe23bc49184d8e0635ebf1b95
quedevel <kdh90817@naver.com>
2022-05-11 14:49:11 +0900

    Question19

--- que-java-algorithm/src/section2/Question19.java
+++ que-java-algorithm/src/section2/Question19.java
@@ -1,8 +1,10 @@
+package section2;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Question19 {
     public static void solution(List<Integer> list){
         int point = 0;
         int sum = 0;
