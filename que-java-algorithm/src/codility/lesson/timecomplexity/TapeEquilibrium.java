11aea27b2a186ae30520a3c03e647f0f608d375f
quedevel <kdh90817@naver.com>
2022-06-15 13:35:38 +0900

    [codility] TapeEquilibrium

---
+++ que-java-algorithm/src/codility/lesson/timecomplexity/TapeEquilibrium.java
@@ -0,0 +1,28 @@
+package codility.lesson.timecomplexity;
+
+public class TapeEquilibrium {
+
+//    N is an integer within the range [2..100,000];
+//    each element of array A is an integer within the range [−1,000..1,000].
+    public int solution(int[] A) {
+        int sum = 0;
+        for (int num : A) {
+            sum += num;
+        }
+        int left = 0;
+        int right = 0;
+        int min = 100000;
+        for (int i = 0; i < A.length-1; i++) {
+            left += A[i];
+            right = sum - left;
+            int abs = Math.abs(left - right);
+            if (abs < min) min = abs;
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        TapeEquilibrium t = new TapeEquilibrium();
+        System.out.println(t.solution(new int[]{1,2,3,4,5}));
+    }
+}
