d15d6eae6986dfb1cfc87f03c1bfe793de49270e
quedevel <kdh90817@naver.com>
2022-06-15 11:09:52 +0900

    [codility] FrogJmp

---
+++ que-java-algorithm/src/codility/lesson/timecomplexity/FrogJmp.java
@@ -0,0 +1,15 @@
+package codility.lesson.timecomplexity;
+
+public class FrogJmp {
+    public static int solution(int X, int Y, int D) {
+        int add = 0;
+        int quotient = (Y-X)/D;
+        int remainder = (Y-X)%D;
+        if (remainder > 0) add = 1;
+        return quotient + add;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(1, 1000000, 30));
+    }
+}
