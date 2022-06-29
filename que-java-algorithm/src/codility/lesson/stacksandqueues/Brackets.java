26e87fedcb7be7034cfd66e25e31659f33445cf5
quedevel <kdh90817@naver.com>
2022-06-29 23:01:12 +0900

    [codility] Brackets

---
+++ que-java-algorithm/src/codility/lesson/stacksandqueues/Brackets.java
@@ -0,0 +1,33 @@
+package codility.lesson.stacksandqueues;
+
+import java.util.Stack;
+
+public class Brackets {
+//    N is an integer within the range [0..200,000];
+//    string S consists only of the following characters: "(", "{", "[", "]", "}" and/or ")".
+    public int solution(String S) {
+        if (S.length()%2 != 0) return 0;
+        Stack<Character> stack = new Stack<>();
+        char[] chars = S.toCharArray();
+        for (char c : chars) {
+            if (c == '(' || c == '{' || c == '['){
+                stack.push(c);
+                continue;
+            } else if (c == ')') {
+                if (stack.isEmpty()) return 0;
+                if ('(' == stack.peek()) stack.pop();
+            } else if (c == '}') {
+                if (stack.isEmpty()) return 0;
+                if ('{' == stack.peek()) stack.pop();
+            } else if (c == ']') {
+                if (stack.isEmpty()) return 0;
+                if ('[' == stack.peek()) stack.pop();
+            }
+        }
+        return stack.isEmpty()? 1 : 0;
+    }
+    public static void main(String[] args) {
+        Brackets b = new Brackets();
+        System.out.println(b.solution(""));
+    }
+}
