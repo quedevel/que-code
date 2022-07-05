e95c2b842585fcf5a5a513e1c4eb99bdda39965a
quedevel <kdh90817@naver.com>
2022-07-05 14:13:39 +0900

    Question3_5 연속된 자연수의 합

--- que-java-algorithm/src/section3/Question3_5.java
+++ que-java-algorithm/src/section3/Question3_5.java
@@ -7,19 +7,26 @@ import java.util.Scanner;
  * 5. 연속된 자연수의 합
  */
 public class Question3_5 {
-    public static void solution(int cnt, int[] iArr){
-        int max = 0;
-
-        System.out.println(max);
+    public static void solution(int num){
+        int result = 0;
+        int maxLength = num%2 == 0? num/2 : num/2+1;
+        for (int i = 1; i <= maxLength; i++) {
+            int sum = 0;
+            for (int j = i; j <= maxLength; j++) {
+                sum += j;
+                if (sum == num){
+                    result++;
+                    break;
+                } else if (sum > num){
+                    break;
+                }
+            }
+        }
+        System.out.println(result);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int total = sc.nextInt();
-        int cnt = sc.nextInt();
-        int[] iArr = new int[total];
-        for (int i = 0; i < total; i++) {
-            iArr[i] = sc.nextInt();
-        }
-        solution(cnt, iArr);
+        int num = sc.nextInt();
+        solution(num);
     }
 }
