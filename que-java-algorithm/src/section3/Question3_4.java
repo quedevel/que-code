d69cb76bc78c4b89f20a8b54dca55705420d70b8
quedevel <kdh90817@naver.com>
2022-07-05 13:27:41 +0900

    Question3_4 연속 부분수열

--- que-java-algorithm/src/section3/Question3_4.java
+++ que-java-algorithm/src/section3/Question3_4.java
@@ -1,5 +1,6 @@
 package section3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -7,19 +8,31 @@ import java.util.Scanner;
  * 4. 연속 부분수열
  */
 public class Question3_4 {
-    public static void solution(int cnt, int[] iArr){
-        int max = 0;
-
-        System.out.println(max);
+    public static void solution(int goal, int[] iArr){
+        int cnt = 0;
+        int result = 0;
+        for (int i = 0; i < iArr.length; i++) {
+            cnt = 0;
+            for (int j = i; j < iArr.length; j++) {
+                cnt += iArr[j];
+                if (cnt == goal){
+                    result++;
+                    break;
+                } else if (cnt > goal){
+                    break;
+                }
+            }
+        }
+        System.out.println(result);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int total = sc.nextInt();
-        int cnt = sc.nextInt();
-        int[] iArr = new int[total];
-        for (int i = 0; i < total; i++) {
+        int length = sc.nextInt();
+        int goal = sc.nextInt();
+        int[] iArr = new int[length];
+        for (int i = 0; i < length; i++) {
             iArr[i] = sc.nextInt();
         }
-        solution(cnt, iArr);
+        solution(goal, iArr);
     }
 }
