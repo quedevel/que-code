fb320f1f1d677f8738dafdc2f9f25f9690215b6f
quedevel <kdh90817@naver.com>
2022-07-06 10:00:58 +0900

    Question3_6 최대 길이 연속부분수열(복합적 문제)

--- que-java-algorithm/docs/UNRESOLVED_PROBLEMS.md
+++ que-java-algorithm/docs/UNRESOLVED_PROBLEMS.md
@@ -1,5 +1,6 @@
 ## 해결하지 못한 문제 리스트
 
-1. Question2_11
+1. Question2_11 
 2. Question2_12
 3. Question3_3
+4. Question3_6
--- que-java-algorithm/src/section3/Question3_6.java
+++ que-java-algorithm/src/section3/Question3_6.java
@@ -4,22 +4,29 @@ import java.util.Scanner;
 
 /**
  * 03-06
- * 6. 최대 길이 연속부분수열
+ * 6. 최대 길이 연속부분수열(복합적 문제)
  */
 public class Question3_6 {
-    public static void solution(int cnt, int[] iArr){
-        int max = 0;
-
-        System.out.println(max);
+    public static void solution(int l,int k, int[] arr){
+        int result = 0, cnt = 0, lt = 0;
+        for (int rt = 0; rt < l; rt++) {
+            if (arr[rt] == 0) cnt++;
+            while(cnt > k){
+                if (arr[lt] == 0) cnt--;
+                lt++;
+            }
+            result = Math.max(result, rt-lt+1);
+        }
+        System.out.println(result);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int total = sc.nextInt();
-        int cnt = sc.nextInt();
-        int[] iArr = new int[total];
-        for (int i = 0; i < total; i++) {
-            iArr[i] = sc.nextInt();
+        int l = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[l];
+        for (int i = 0; i < l; i++) {
+            arr[i] = sc.nextInt();
         }
-        solution(cnt, iArr);
+        solution(l, k, arr);
     }
 }
