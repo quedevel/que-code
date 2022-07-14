c426ff466e2541004ca1faf51963e8ef93f5f3c0
quedevel <kdh90817@naver.com>
2022-07-14 23:12:16 +0900

    Clean Code init

---
+++ que-clean-code/src/main/java/clean/code/QueCleanCodeApplication.java
@@ -0,0 +1,13 @@
+package clean.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueCleanCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueCleanCodeApplication.class, args);
+    }
+
+}
---
+++ que-clean-code/src/test/java/clean/code/QueCleanCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package clean.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueCleanCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
