4340bc8c06228b3a8a281d35dd148b5de434ffc3
quedevel <kdh90817@naver.com>
2022-07-15 14:37:05 +0900

    Question5_3 크레인 인형뽑기(카카오)

---
+++ que-java-algorithm/src/section5/Question5_3.java
@@ -0,0 +1,47 @@
+package section5;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * 05-03
+ * 3. 크레인 인형뽑기(카카오)
+ */
+public class Question5_3 {
+    public static void solution(int n, int[][] board, int m, int[] moves){
+        int cnt = 0;
+        Stack<Integer> container = new Stack<>();
+        for (int i = 0; i < m; i++) {
+            int column = moves[i];
+            for (int j = 0; j < n; j++) {
+                if (board[j][column] != 0){
+                    if (!container.isEmpty() && board[j][column] == container.peek()){
+                        container.pop();
+                        cnt += 2;
+                    } else {
+                        container.push(board[j][column]);
+                    }
+                    board[j][column] = 0;
+                    break;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+        int m = sc.nextInt();
+        int[] moves = new int[m];
+        for (int i = 0; i < m; i++) {
+            moves[i] = sc.nextInt()-1;
+        }
+        solution(n, board, m, moves);
+    }
+}
