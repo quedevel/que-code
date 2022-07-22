d97af6fe8d5b9f7b323fc4db7c80d4a5b78274f2
quedevel <kdh90817@naver.com>
2022-07-22 16:30:08 +0900

    cs 스터디 init

---
+++ que-computer-science/src/main/java/computer/science/QueComputerScienceApplication.java
@@ -0,0 +1,13 @@
+package computer.science;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueComputerScienceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueComputerScienceApplication.class, args);
+    }
+
+}
---
+++ que-computer-science/src/test/java/computer/science/QueComputerScienceApplicationTests.java
@@ -0,0 +1,13 @@
+package computer.science;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueComputerScienceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
