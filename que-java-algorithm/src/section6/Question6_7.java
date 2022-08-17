adaa0ce7b161f406d6f3a25919486d9c9ee3fc50
quedevel <kdh90817@naver.com>
2022-08-17 11:10:57 +0900

    Question6_7 좌표 정렬

--- que-java-algorithm/src/section6/Question6_7.java
+++ que-java-algorithm/src/section6/Question6_7.java
@@ -1,6 +1,12 @@
+package section6;
+
 import java.util.Scanner;
 
-public class Main {
+/**
+ * 06-07
+ * 7. 좌표 정렬
+ */
+public class Question6_7 {
     public static int[][] solution(int[][] arr){
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
@@ -9,11 +15,11 @@ public class Main {
                     arr[j] = arr[i];
                     arr[i] = temp;
                 } else if (arr[j][0] == arr[i][0]) {
-                    if (arr[j][1] < arr[i][1]){
-                        int[] temp = arr[j];
-                        arr[j] = arr[i];
-                        arr[i] = temp;
-                    }
+                   if (arr[j][1] < arr[i][1]){
+                       int[] temp = arr[j];
+                       arr[j] = arr[i];
+                       arr[i] = temp;
+                   }
                 }
             }
         }
