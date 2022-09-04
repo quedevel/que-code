9cbfc7b57b05403e2e18f1e647e14d3ef676fa5d
quedevel <kdh90817@naver.com>
2022-09-04 12:49:08 +0900

    [노원 스터디] jpa init

---
+++ nowon-jpa/src/main/java/hello/nowonjpa/NowonJpaApplication.java
@@ -0,0 +1,13 @@
+package hello.nowonjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowonJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NowonJpaApplication.class, args);
+    }
+
+}
---
+++ nowon-jpa/src/test/java/hello/nowonjpa/NowonJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.nowonjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowonJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
