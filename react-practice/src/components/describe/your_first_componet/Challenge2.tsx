b296ca20cfc6b770981e19145b650ad1b21b094c
quedevel <kdh90817@naver.com>
2023-05-11 16:40:15 +0900

    :sparkles: feat: I First Component
    ## I will learn
    * What a component is
    * What role components play in a React application
    * How to write your first React component

---
+++ react-practice/src/components/describe/your_first_componet/Challenge2.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Profile() {
+    return (<img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />)
+}
---
+++ react-practice/src/components/describe/your_first_componet/Challenge4.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Congratulations(){
+    return (<h1>Good job!</h1>)
+}
