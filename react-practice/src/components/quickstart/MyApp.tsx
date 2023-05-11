92ac2ca3b3c0345ae60b365062d400f671c7de2c
quedevel <kdh90817@naver.com>
2023-05-11 14:13:55 +0900

    :zap: update: 세미콜론 제거

--- react-practice/src/components/quickstart/MyApp.tsx
+++ react-practice/src/components/quickstart/MyApp.tsx
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState} from 'react'
 import MyButton from './MyButton'
-import Profile from "./Profile";
-import ShoppingList from "./ShoppingList";
+import Profile from "./Profile"
+import ShoppingList from "./ShoppingList"
 
 export default function MyApp(){
     const [count, setCount] = useState<number>(0)
 
     const handleClick = (): void => {
-        setCount(count + 1);
+        setCount(count + 1)
     }
 
     return (
--- react-practice/src/components/quickstart/MyButton.tsx
+++ react-practice/src/components/quickstart/MyButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 interface MyButtonProps{
     count: number
@@ -10,5 +10,5 @@ export default function MyButton({ count, onClick }: MyButtonProps) {
         <button onClick={onClick}>
             Clicked {count} times
         </button>
-    );
+    )
 }
