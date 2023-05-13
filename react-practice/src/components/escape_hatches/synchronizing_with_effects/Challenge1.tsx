121ebf3506b08b6aa6def2f579fc4ba2b77bbecc
quedevel <kdh90817@naver.com>
2023-05-13 19:39:56 +0900

    :sparkles: feat: Synchronizing with Effects
    I will learn
    * What Effects are
    * How Effects are different from events
    * How to declare an Effect in your component
    * How to skip re-running an Effect unnecessarily
    * Why Effects run twice in development and how to fix them

---
+++ react-practice/src/components/escape_hatches/synchronizing_with_effects/Challenge1.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useRef } from 'react';
+
+type MyInputProps = {
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function MyInput({ value, onChange }: MyInputProps) {
+    const ref = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (ref.current) ref.current.focus()
+    }, []);
+
+    return (
+        <input
+            ref={ref}
+            value={value}
+            onChange={onChange}
+        />
+    );
+}
---
+++ react-practice/src/components/escape_hatches/synchronizing_with_effects/Challenge2.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef } from 'react';
+
+interface MyInputProps {
+    shouldFocus: boolean;
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function MyInput({ shouldFocus, value, onChange }: MyInputProps) {
+    const ref = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (shouldFocus && ref.current) {
+            ref.current.focus();
+        }
+    }, [shouldFocus]);
+
+    return (
+        <input
+            ref={ref}
+            value={value}
+            onChange={onChange}
+        />
+    );
+}
