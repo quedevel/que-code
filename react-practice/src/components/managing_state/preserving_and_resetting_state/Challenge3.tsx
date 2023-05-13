22fd0b427413fcada67d7cd67b085bb779d8c75a
quedevel <kdh90817@naver.com>
2023-05-13 15:51:13 +0900

    :sparkles: feat: Preserving and Resetting State
    I will learn
    * How React “sees” component structures
    * When React chooses to preserve or reset the state
    * How to force React to reset component’s state
    * How keys and types affect whether the state is preserved

---
+++ react-practice/src/components/managing_state/preserving_and_resetting_state/Challenge3.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import ContactList, {Contact} from './ContactList';
+import EditContact from './EditContact';
+
+export default function ContactManager() {
+    const [
+        contacts,
+        setContacts
+    ] = useState<Contact[]>(initialContacts);
+    const [
+        selectedId,
+        setSelectedId
+    ] = useState<number>(0);
+    const selectedContact = contacts.find(c =>
+        c.id === selectedId
+    );
+
+    function handleSave(updatedData: Contact) {
+        const nextContacts = contacts.map(c => {
+            if (c.id === updatedData.id) {
+                return updatedData;
+            } else {
+                return c;
+            }
+        });
+        setContacts(nextContacts);
+    }
+
+    return (
+        <div>
+            <ContactList
+                contacts={contacts}
+                selectedId={selectedId}
+                onSelect={(id: number)=> setSelectedId(id)}
+            />
+            <hr />
+            {selectedContact && (
+                <EditContact
+                    key={selectedId}
+                    initialData={selectedContact}
+                    onSave={handleSave}
+                />
+            )}
+        </div>
+    )
+}
+
+const initialContacts: Contact[] = [
+    { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
+    { id: 1, name: 'Alice', email: 'alice@mail.com' },
+    { id: 2, name: 'Bob', email: 'bob@mail.com' }
+];
---
+++ react-practice/src/components/managing_state/preserving_and_resetting_state/Contact.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+type Contact = {
+    id: number
+    name: string
+    email: string
+}
+
+export default function Contact({ contact }: {contact: Contact}) {
+    const [expanded, setExpanded] = useState(false);
+    return (
+        <>
+            <p><b>{contact.name}</b></p>
+            {expanded &&
+                <p><i>{contact.email}</i></p>
+            }
+            <button onClick={() => {
+                setExpanded(!expanded);
+            }}>
+                {expanded ? 'Hide' : 'Show'} email
+            </button>
+        </>
+    );
+}
