d29c102bb56efdb624667784960d128c28b8ad3a
quedevel <kdh90817@naver.com>
2023-06-29 15:04:34 +0900

    feat: 스프링 시큐리티 보일러 플레이트 제작할 프로젝트

---
+++ que-spring-security/src/main/java/dev/sec/QueSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package dev.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ que-spring-security/src/test/java/dev/sec/QueSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
